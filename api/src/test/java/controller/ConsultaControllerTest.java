2fd96af4d254fe6099453a387553acff0684ba9f
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-01-03 13:47:32 -0600

    pon chang

--- api/src/test/java/controller/ConsultaControllerTest.java
+++ api/src/test/java/controller/ConsultaControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -38,7 +39,7 @@ class ConsultaControllerTest {
     @Autowired
     private JacksonTester<DatosDetalleConsulta> datosDetalleConsultaJson;
 
-    @Mock
+    @MockitoBean
     private ReservaDeConsultas reservaDeConsultas;
 
     @Test
